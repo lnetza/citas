1071dd34210c4a55059159760dd981d0b148a693
Netza López <l.lopeznetza@gmail.com>
2020-01-23 13:12:43 -0600

    Campos de formulario
    Se agregan campos de tipo fecha, texto, texarea etc

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 
 const Formulario =()=>{
     return (
-        <h1>Desde formulario</h1>
+        <Fragment>
+            <h2>Crear cita</h2>
+
+            <form>
+                <label>Nombre mascota</label>
+                <input
+                    type="text"
+                    name="mascota"
+                    className="u-full-width"
+                    placeholder="Nombre mascota"
+                />
+                <label>Nombre dueño</label>
+                <input
+                    type="text"
+                    name="propietario"
+                    className="u-full-width"
+                    placeholder="Nombre dueño de la mascota"
+                />
+                <label>Fecha</label>
+                <input
+                    type="date"
+                    name="fecha"
+                    className="u-full-width"
+                />
+
+                <label>Hora</label>
+                <input
+                    type="time"
+                    name="hora"
+                    className="u-full-width"
+                />
+
+                <label>Síntoma</label>
+                <textarea
+                    className="u-full-width"
+                    name="sintomas"
+                ></textarea>
+                <button 
+                    type="submit"
+                    className="u-full-width button-primary"
+                >Guardar cita</button>
+            </form>
+        </Fragment>
     );
 }
 
